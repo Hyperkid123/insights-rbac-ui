9eca1ea3e8edd6f3ff81f980d1d4f73c2c3d2280
Martin Marosi <marvusm.mmi@gmail.com>
2020-10-12 11:54:22 +0200

    Modify spandex to load chrome 2.0

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -5,14 +5,14 @@ const APP_ID = 'rbac';
 const APP_ID2 = 'my-user-access'
 const routes = {};
 
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/beta/apps/${APP_ID}`] = { host: 'http://localhost:8002' };
-routes[`/apps/${APP_ID}`] = { host: 'http://localhost:8002' };
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8080' };
+routes[`/${SECTION}/${APP_ID}`] = { host: 'http://localhost:8080' };
+routes[`/beta/apps/${APP_ID}`] = { host: 'http://localhost:8080' };
+routes[`/apps/${APP_ID}`] = { host: 'http://localhost:8080' };
 
-routes[`/beta/${SECTION}/${APP_ID2}`] = { host: 'http://localhost:8002' };
-routes[`/${SECTION}/${APP_ID2}`] = { host: 'http://localhost:8002' };
-routes[`/beta/apps/${APP_ID2}`] = { host: 'http://localhost:8002' };
-routes[`/apps/${APP_ID2}`] = { host: 'http://localhost:8002' };
+routes[`/beta/${SECTION}/${APP_ID2}`] = { host: 'http://localhost:8080' };
+routes[`/${SECTION}/${APP_ID2}`] = { host: 'http://localhost:8080' };
+routes[`/beta/apps/${APP_ID2}`] = { host: 'http://localhost:8080' };
+routes[`/apps/${APP_ID2}`] = { host: 'http://localhost:8080' };
 
 module.exports = { routes };
