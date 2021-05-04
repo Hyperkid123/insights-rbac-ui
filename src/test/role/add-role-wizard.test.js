acbc74cd482d9a8023024b820d97358b13685a53
Martin Marosi <marvusm.mmi@gmail.com>
2021-05-04 14:05:31 +0200

    Update tests

--- src/test/role/add-role-wizard.test.js
+++ src/test/role/add-role-wizard.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
@@ -40,27 +41,32 @@ describe('<AddRoleWizard />', () => {
     mockStore = configureStore(middlewares);
   });
 
-  it('should render correctly', () => {
+  it('should render correctly', async () => {
     const store = mockStore(initialState);
-    const wrapper = shallow(
-      <RoleWrapper store={store}>
-        <AddRoleWizard {...initialProps} />
-      </RoleWrapper>
-    ).dive();
-
-    setImmediate(() => {
-      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    let wrapper;
+    await act(async () => {
+      wrapper = shallow(
+        <RoleWrapper store={store}>
+          <AddRoleWizard {...initialProps} />
+        </RoleWrapper>
+      ).dive();
     });
+
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 
-  it('should show a warning modal on Cancel', () => {
+  it('should show a warning modal on Cancel', async () => {
     const store = mockStore(initialState);
 
-    const wrapper = mount(
-      <RoleWrapper store={store}>
-        <Route path="/roles/add-role/" render={() => <AddRoleWizard {...initialProps} />} />
-      </RoleWrapper>
-    );
+    let wrapper;
+    await act(async () => {
+      wrapper = mount(
+        <RoleWrapper store={store}>
+          <Route path="/roles/add-role/" render={() => <AddRoleWizard {...initialProps} />} />
+        </RoleWrapper>
+      );
+    });
+    wrapper.update();
     expect(wrapper.find(WarningModal).getElement().props.isOpen).not.toBeTruthy();
     const input = wrapper.find('input#name');
     input.getDOMNode().value = 'foo';
@@ -71,29 +77,37 @@ describe('<AddRoleWizard />', () => {
     expect(wrapper.find(WarningModal).getElement().props.isOpen).toBeTruthy();
   });
 
-  it('should not show a warning modal on Cancel when clean', (done) => {
+  it('should not show a warning modal on Cancel when clean', async () => {
     const store = mockStore(initialState);
 
-    const wrapper = mount(
-      <RoleWrapper store={store}>
-        <Route path="/roles/add-role/" render={() => <AddRoleWizard {...initialProps} />} />
-      </RoleWrapper>
-    );
+    let wrapper;
+    await act(async () => {
+      wrapper = mount(
+        <RoleWrapper store={store}>
+          <Route path="/roles/add-role/" render={() => <AddRoleWizard {...initialProps} />} />
+        </RoleWrapper>
+      );
+    });
     const expectedActions = expect.arrayContaining([
       expect.objectContaining({
         type: ADD_NOTIFICATION,
         payload: expect.objectContaining({ title: 'Creating role was canceled by the user', variant: 'warning' }),
       }),
     ]);
-    expect(wrapper.find(WarningModal).getElement().props.isOpen).not.toBeTruthy();
-    wrapper.find('.pf-c-button.pf-m-primary').simulate('click');
     wrapper.update();
-    wrapper.find('.pf-m-link').simulate('click');
+    expect(wrapper.find(WarningModal).getElement().props.isOpen).not.toBeTruthy();
 
-    setImmediate(() => {
-      expect(store.getActions()).toEqual(expectedActions);
-      expect(wrapper.find(WarningModal)).toHaveLength(0);
-      done();
+    act(() => {
+      wrapper.find('.pf-c-button.pf-m-primary').simulate('click');
     });
+    wrapper.update();
+
+    act(() => {
+      wrapper.find('.pf-m-link').simulate('click');
+    });
+
+    wrapper.update();
+    expect(store.getActions()).toEqual(expectedActions);
+    expect(wrapper.find(WarningModal)).toHaveLength(0);
   });
 });
